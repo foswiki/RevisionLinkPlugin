cc63f1233d68fa2eddd9110733a995dfd18cd54b
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2008-12-14 17:49:56 +0000

    Item27: Fix svn properties on all .pm, .tmpl and .txt files
    git-svn-id: http://svn.foswiki.org/trunk/RevisionLinkPlugin@1340 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/TWiki/Plugins/RevisionLinkPlugin.pm
+++ lib/TWiki/Plugins/RevisionLinkPlugin.pm
@@ -39,10 +39,10 @@ use strict;
 use vars qw( $VERSION $RELEASE $debug $pluginName );
 #$web $topic $user $installWeb
 
-# This should always be $Rev: 12903 $ so that TWiki can determine the checked-in
+# This should always be $Rev$ so that TWiki can determine the checked-in
 # status of the plugin. It is used by the build automation tools, so
 # you should leave it alone.
-$VERSION = '2.1 ($Rev: 12903 $)';
+$VERSION = '2.1 ($Rev$)';
 
 # This is a free-form string you can use to "name" your own plugin version.
 # It is *not* used by the build automation tools, but is reported as part
